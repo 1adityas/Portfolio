01845cbcc8cdbb2980e2c138d0d0611f0a2f08fe
aditya pratap singh <1adityapsingh@gmail.com>
2022-08-30 11:22:21 +0530

    added assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import Header from "./layouts/header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <div className="body">
+        <div className="imgBack">
+          <div className="adiImg"></div>
+        </div>
+        <div className="intro">
+          <p id="top">Hi There,</p>
+          <p id="mid">I'm Aditya Pratap Singh</p>
+          <p id="body">
+            I'm Junior Software Developer with a passion for everything from
+            frontend to backend
+            <br />
+            to exploring new Tech. Welcome to my corner of Internet.
+            <br />I am Glad you are here.
+          </p>
+        </div>
+      </div>
+      <div class="content">
+        aasdf
+        <div className="container"></div>
+      </div>
+    </>
+  );
 }
 //alright then checking git
 export default App;
---
+++ src/layouts/header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+function Header() {
+  return <div className="App">yooo</div>;
+}
+//alright then checking git
+export default Header;
