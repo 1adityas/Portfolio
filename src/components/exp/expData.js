301b0a371faf0a358d65c8573a7b72ea83145334
aditya pratap singh <1adityapsingh@gmail.com>
2022-09-05 00:47:07 +0530

    made responsive changes in css

--- src/components/exp/expData.js
+++ src/components/exp/expData.js
@@ -4,21 +4,21 @@ const data = [
     img: "srmmate.png",
     body: "A lean and elegant utility to provide students at SRM-IST, NCR withaccess to class notes, reference books and more in just few taps.Built using Flutter, SRM Mate works on Android, iOS, and Windows andoffers steady performance.",
     id: "gd1",
-    color:"rgba(250, 235, 215, 0.474)"
+    color: "rgba(250, 215, 242, 0.474)",
   },
   {
     heading: "Starbucks Clone",
     img: "starbuck.png",
-    body: "A lean and elegant utility to provide students at SRM-IST, NCR withaccess to class notes, reference books and more in just few taps.Built using Flutter, SRM Mate works on Android, iOS, and Windows andoffers steady performance.",
+    body: "Created StarBucks (one of my fav place to sit and work on my laptop).As an attempt to practive and master HTML, CSS, Javascript and to create a dynamic functional website I created a clone of StarBucks Site ",
     id: "gd2",
-    color:"rgba(215, 250, 217, 0.474)"
+    color: "rgba(215, 250, 217, 0.474)",
   },
-  {
-    heading: "Web-scraping ",
-    img: "scraping.png",
-    body: "A lean and elegant utility to provide students at SRM-IST, NCR withaccess to class notes, reference books and more in just few taps.Built using Flutter, SRM Mate works on Android, iOS, and Windows andoffers steady performance.",
-    id: "gd3",
-    color:"rgba(250, 215, 242, 0.474)"
-  },
+  // {
+  //   heading: "Web-scraping ",
+  //   img: "scraping.png",
+  //   body: "A lean and elegant utility to provide students at SRM-IST, NCR withaccess to class notes, reference books and more in just few taps.Built using Flutter, SRM Mate works on Android, iOS, and Windows andoffers steady performance.",
+  //   id: "gd3",
+  //   color:"rgba(250, 215, 242, 0.474)"
+  // },
 ];
 export default data;
--- src/components/timeline/timelineElements.js
+++ src/components/timeline/timelineElements.js
@@ -1,64 +1,54 @@
 let timelineElements = [
-    {
-      id: 1,
-      title: "Frontend Developer",
-      location: "Dragontail, Ascana",
-      description:
-        "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
-      buttonText: "View Frontend Projects",
-      date: "August 2016 - present",
-      icon: "work",
-    },
-    {
-      id: 2,
-      title: "Backend Developer",
-      location: "Skystead, Craonia",
-      description:
-        "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
-      buttonText: "View Backend Projects",
-      date: "June 2013 - August 2016",
-      icon: "work",
-    },
-    {
-      id: 3,
-      title: "Quality Assurance Engineer",
-      location: "South Warren, Geshington",
-      description:
-        "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
-      buttonText: "Company Website",
-      date: "September 2011 - June 2013",
-      icon: "work",
-    },
-    {
-      id: 4,
-      title: "Oak Ridge College",
-      location: "South Warren, Geshington",
-      description:
-        "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      buttonText: "Course Certificate",
-      date: "September 2011",
-      icon: "school",
-    },
-    {
-      id: 5,
-      title: "Hawking College",
-      location: "Skystead, Craonia",
-      description:
-        "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      buttonText: "College Projects",
-      date: "2007 - 2011",
-      icon: "school",
-    },
-    {
-      id: 6,
-      title: "Marble Hills Grammar School",
-      location: "Dragontail, Ascana",
-      description:
-        "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      date: "2003 - 2007",
-      icon: "school",
-    },
-  ];
-  
-  export default timelineElements;
-  
+  {
+    id: 1,
+    title: "Software Developer-Traniee at Technovert",
+    location: "Hyderabad, India",
+    description:
+      "Working actively on a project from a Caribbean client. Making the admin panel screens such as Dashboard and other screens from scratch using Html, SCSS, Angular framework, chartJs library etc.",
+    // buttonText: "View Frontend Projects",
+    date: "March 2022 - present",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer at Crayon & Co",
+    location: "Remote, India",
+    description:
+      "Worked on building function Website from the given UX Screens using React Js a Hospital Management System during Covid peek.",
+    // buttonText: "View Backend Projects",
+    date: "October 2021 - January 2022",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Teaching Assistant at Coding Ninja",
+    location: "Remote, India",
+    description:
+      "Helped students with their coding-related doubts.and had an overall rating of 4.7 out of 5/",
+    // buttonText: "Company Website",
+    date: "October 2021 - December 2013",
+    icon: "school",
+  },
+  {
+    id: 4,
+    title: "Software Developer Intern at Coforge",
+    location: "Noida, India ",
+    description:
+      "worked on python like Web Scrapping, automate reading Excel file and Performing operations and saving as CSV.",
+    // buttonText: "Course Certificate",
+    date: "December 2020 - May 2021",
+    icon: "school",
+  },
+  {
+    id: 5,
+    title: "Official Lead at Codechef Chapter",
+    location: "SRM-NCR, Delhi-NCR, India",
+    description:
+      "Codechef chapter SRM is programming clubs run and maintained by the official chapter leaders and mentored by CodeChef a competitive programming site. -Our team organizes events, meetups, and speaker sessions on campus to raise awareness, hosting various contests and competitions to facilitate learning and conducting programming workshops and doubt-sessions for better learning.",
+    // buttonText: "College Projects",
+    date: "August 2020 - November 2021",
+    icon: "school",
+  },
+];
+
+export default timelineElements;
